b1b27b196834f62200a2f1ac1b57d9cefdc39a64
lacunadream <d.s.yong@lse.ac.uk>
2015-12-10 05:20:23 +0000

    q2 part2!

--- d2.py
+++ d2.py
@@ -8,29 +8,46 @@ pattern = re.compile(r'\d*')
 # # k = int(a) * int(b)
 # # print(k)
 
-totalArea = 0
+# # Part I
+# totalArea = 0
+
+# for line in raw: 
+# 	spec = pattern.findall(line)
+# 	# print(line)
+# 	# print(spec)
+# 	a = int(spec[0])
+# 	b = int(spec[2])
+# 	c = int(spec[4])
+# 	first = a*b
+# 	second = b*c
+# 	third = c*a
+# 	add = min(first, second, third)
+# 	# print(add)
+# 	totalArea = totalArea + 2*first + 2*second + 2*third + add
+# 	print("Total area is %s" % totalArea)
+# 	# print(a)
+# 	# print(b)
+# 	# print(c)
+
+# 	# nice = map(float, spec)  --> doesnt work because 
+# 	# print(nice)
+# 	# print(min(nice))
+
+# 	# print('lol')
+	
+# Part II
+totalRibbon = 0
 
 for line in raw: 
 	spec = pattern.findall(line)
-	# print(line)
-	# print(spec)
 	a = int(spec[0])
 	b = int(spec[2])
 	c = int(spec[4])
-	first = a*b
-	second = b*c
-	third = c*a
-	add = min(first, second, third)
-	# print(add)
-	totalArea = totalArea + 2*first + 2*second + 2*third + add
-	print("Total area is %s" % totalArea)
-	# print(a)
-	# print(b)
-	# print(c)
+	volume = a*b*c
 
-	# nice = map(float, spec)
-	# print(nice)
-	# print(min(nice))
-
-	# print('lol')
-	
+	first = 2*(a+b)
+	second = 2*(b+c)
+	third = 2*(a+c)
+	perimeter = min(first, second, third)
+	totalRibbon = totalRibbon + volume + perimeter
+	print(totalRibbon)
