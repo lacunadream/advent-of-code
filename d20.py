06abe70c866a019822ea9a2a621a9243094b698b
lacunadream <d.s.yong@lse.ac.uk>
2015-12-20 13:32:18 -0500

    time complexity too high though

--- d20.py
+++ d20.py
@@ -1,9 +1,9 @@
-data = 18
-raw = 20
+data = 3400000
 bar = 1
 multiAdd = 0
+flag = 0
 
-for i in range (1, raw):
+for i in range (1, data):
 	bar += 1
 	print ("initial %s" %i)
 	for k in range (1, bar):
@@ -12,5 +12,9 @@ for i in range (1, raw):
 			print(multiAdd)
 			if multiAdd == data:
 				print ("***")
+				flag = 1
 				break 
+	if (flag == 1):
+		print ("Answer is %s" %i)
+		break  
 	multiAdd = 0
